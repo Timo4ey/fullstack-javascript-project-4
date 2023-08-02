2bc7ddfdec6544f41a947a29a669a5eec66d66ef
Timofey <tima_yakovishin@mail.ru>
2023-08-02 16:42:26 +0300

    [fix] fix lint misstakes :

--- __tests__/errorHandler.test.js
+++ __tests__/errorHandler.test.js
@@ -1,11 +1,6 @@
 import errorHandler from '../src/errorHandlers/errorHandler.js';
-import axiosInt from '../src/debug/debuger.js';
-import nock from 'nock';
-import getDom from '../src/loaders/getDom.js';
 
-axiosInt();
-
-test('Should return an exception if pass an argument', async () => {
+test("Should'nt return  return an exception if pass an argument", async () => {
   expect.assertions(0);
   try {
     errorHandler('');
@@ -14,11 +9,7 @@ test('Should return an exception if pass an argument', async () => {
   }
 });
 
-test('Test function getData.  Should return a list of entries [[filename.format, data]]', async () => {
-  const targetUrl = 'https://ru.hexlet.io/courses';
-  // const expected = await fsp.readFile(path.join('var', 'after', 'ru-hexlet-io3.html'), 'utf-8');
-
-  nock('https://ru.hexlet.io').get('/courses').reply(404, 'Bad requst');
+test('Should return an exception if pass an argument', async () => {
   expect.assertions(0);
   try {
     errorHandler('');
