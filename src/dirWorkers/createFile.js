64d7753f1dce8e4a6066ba046ba435616c97491b
Timofey <tima_yakovishin@mail.ru>
2023-08-03 18:18:19 +0300

    [fix] fix dataLoader.js #17

--- src/dirWorkers/createFile.js
+++ src/dirWorkers/createFile.js
@@ -1,14 +1,18 @@
 import fsp from 'fs/promises';
 import path from 'path';
+import { checkAccess } from './createDirectory.js';
 
 const createFile = (filename, filepath, data = '') => {
   console.log('!!!', path.join(filepath, filename));
-  return fsp
-    .writeFile(path.join(filepath, filename), data)
-    .catch((error) => {
-      throw new Error(`Error creating file: ${error.message} (${path.join(filepath, filename)})`);
-    })
-    .then(() => `${path.join(filepath, filename)}`);
+  return checkAccess(filepath).then(
+    fsp
+      .writeFile(path.join(filepath, filename), data)
+      .catch((error) => {
+        throw new Error(`Error creating file: ${error.message} (${path.join(filepath, filename)})`);
+      })
+      .then(() => `${path.join(filepath, filename)}`)
+      .catch((err) => console.err(err.message)),
+  );
 };
 
 export default createFile;
