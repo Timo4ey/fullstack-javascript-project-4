b5112399d85b6421f8d339cc21c87f35e03742d7
Timofey <tima_yakovishin@mail.ru>
2023-08-03 10:44:42 +0300

    [fix] fix main file. v3

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -4,21 +4,32 @@ import { Command } from 'commander';
 import dataLoader from '../src/loaders/dataLoader.js';
 
 const program = new Command();
-program.name('page-loader').description('Page loader utility').version('0.0.1');
+// program.name('page-loader').description('Page loader utility').version('0.0.1');
+
+// program
+//   .argument('<url>')
+//   .option('-o, --output [dir]', 'output dir path', process.cwd())
+//   .action((link, dir) => {
+//     console.log(program.opts());
+//     dataLoader(link, dir.output).catch((err) => {
+//       console.error(err);
+//       process.exit(1);
+//     });
+//   });
 
 program
-  .option(
-    '-o, --output [dir]',
-
-    'output dir',
-    process.cwd(),
-  )
-  .argument('<url>')
-  .action((link, dir) =>
-    dataLoader(link, dir.output).catch((err) => {
-      console.error(err);
-      process.exit(1);
-    }),
-  );
+  .description('Page loader utility')
+  .arguments('<url>')
+  .version('0.0.1')
+  .option('-o, --output [dir]', 'output dir path', process.cwd())
+  .action((url) => {
+    const options = program.opts();
+    dataLoader(url, options.output)
+      .then((filePath) => console.log(`Page was successfully downloaded into '${filePath}'`))
+      .catch((err) => {
+        console.error(err);
+        process.exit(1);
+      });
+  });
 
 program.parse(process.argv);
