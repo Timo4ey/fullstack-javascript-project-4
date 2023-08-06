6ba03bc56c890ff3879955752e9c180a17c9f8c5
Timofey <tima_yakovishin@mail.ru>
2023-08-06 10:54:11 +0300

    [fix]  upd ctreate file #26

--- src/dirWorkers/createFile.js
+++ src/dirWorkers/createFile.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { checkAccess } from './createDirectory.js';
 
 const createFile = (filename, filepath, data = '') => {
-  console.log('!!!', path.join(filepath, filename));
+  console.log('!!!', filepath, path.join(filepath, filename));
   return checkAccess(filepath).then(
     fsp
       .writeFile(path.join(filepath, filename), data)
